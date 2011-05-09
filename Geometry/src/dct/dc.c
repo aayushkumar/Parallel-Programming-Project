7363ccd63cdc48f51247ec5b24f4737b7851ce4a
Aayush Kumar <aayushkumar@gmx.com>
2011-05-09 09:55:04 -0400

    Removed the copyright notices on top of each file

--- Geometry/src/dct/dc.c
+++ Geometry/src/dct/dc.c
@@ -1,25 +1,3 @@
-/*   Author: Geoff Leach, Department of Computer Science, RMIT.
- *   email: gl@cs.rmit.edu.au
- *
- *   Date: 6/10/93
- *
- *   Version 1.0
- *   
- *   Copyright (c) RMIT 1993. All rights reserved.
- *
- *   License to copy and use this software purposes is granted provided 
- *   that appropriate credit is given to both RMIT and the author.
- *
- *   License is also granted to make and use derivative works provided
- *   that appropriate credit is given to both RMIT and the author.
- *
- *   RMIT makes no representations concerning either the merchantability 
- *   of this software or the suitability of this software for any particular 
- *   purpose.  It is provided "as is" without express or implied warranty 
- *   of any kind.
- *
- *   These notices must be retained in any copies of any part of this software.
- */
 
 #include  <stddef.h>
 #include  "defs.h"
@@ -116,14 +94,15 @@ static void lower_tangent(edge *r_cw_l, point *s, edge *l_ccw_r, point *u,
   while (!finished)
     /* 
      * This is the case when (roughly speaking) d_l is above o_l and o_r is
-     * above d_l
+     * above d_l.  
      */
     if (Cross_product_3p(o_l, d_l, o_r) > 0.0) {
       l = Prev(l, d_l);
       o_l = d_l;
       d_l = Other_point(l, o_l);
     } 
-    /* This is the case when (roughly speaking) the d_r (dest of right edge)
+    /* 
+     * This is the case when (roughly speaking) the d_r (dest of right edge)
      * is "above" o_r (origin of right edge).  If this is NOT the case then
      * we enter the if clause
      */
